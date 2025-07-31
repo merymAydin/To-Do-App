1fe715823faf6cf3a352c53c392190d0809a7f36
Meryem Aydın <aydnn.meryem@gmail.com>
2025-07-31 17:20:12 +0300

    Refactor to-do list functionality and enhance event handling

--- ToDoApp/toDo.js
+++ ToDoApp/toDo.js
@@ -7,9 +7,36 @@ const searchInput = document.querySelector("#search");
 const todoList = document.querySelector("#todo-list");
 const clearButton = document.querySelector("#clear");
 
-
 loadEventListener();
 function loadEventListener() {
-    form.addEventListener("submit",() => {});
-    addButton.addEventListener("click",() => {});
+  form.addEventListener("submit", addToDo);
 }
+
+function addToDo(e) {
+  const newTodo = todoInput.value.trim();
+  if (newTodo === "") {
+    console.log("Boş değer girilemez...");
+  } else {
+    createToDoListItem(newTodo);
+
+    let todos = localStorage.getItem("todos") ? JSON.parse(localStorage.getItem("todos")): [];
+    return todos;
+  }
+  e.preventDefault();
+}
+
+function createToDoListItem(todo){
+    const todoItem = document.createElement("li");
+    todoItem.className = "list-group-item bg-dark text-light";
+    todoItem.classList.add(
+    "d-flex",
+    "justify-content-between",
+    "align-items-center"
+  );
+  const spanElement = document.createElement("span");
+  spanElement.appendChild(document.createTextNode(todo));
+  todoItem.appendChild(spanElement);
+  todoItem.innerHTML += `<i class="fa-solid fa-trash"></i>`;
+  todoList.appendChild(todoItem);
+}
+
