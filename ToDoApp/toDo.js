7698c6f642add1d8f8666db088d92686a3df239e
Meryem Aydın <aydnn.meryem@gmail.com>
2025-07-31 18:50:56 +0300

    added function to add a todo to localStorage and function to load the app and display existing todos

--- ToDoApp/toDo.js
+++ ToDoApp/toDo.js
@@ -1,3 +1,4 @@
+//assigning HTML elements to variables and using querySelector to select elements by their IDs
 const form = document.querySelector("#todo-form");
 const todoInput = document.querySelector("#todo-input");
 const addButton = document.querySelector("#add");
@@ -10,21 +11,23 @@ const clearButton = document.querySelector("#clear");
 loadEventListener();
 function loadEventListener() {
   form.addEventListener("submit", addToDo);
+  document.addEventListener("DOMContentLoaded", loadApp);
 }
 
+// Function to add a new todo item
 function addToDo(e) {
+  
   const newTodo = todoInput.value.trim();
   if (newTodo === "") {
     console.log("Boş değer girilemez...");
   } else {
     createToDoListItem(newTodo);
-
-    let todos = localStorage.getItem("todos") ? JSON.parse(localStorage.getItem("todos")): [];
-    return todos;
+    addToDoLocalStorage(newTodo);
   }
   e.preventDefault();
 }
 
+// Function to create a new todo list item and append it to the list
 function createToDoListItem(todo){
     const todoItem = document.createElement("li");
     todoItem.className = "list-group-item bg-dark text-light";
@@ -38,5 +41,32 @@ function createToDoListItem(todo){
   todoItem.appendChild(spanElement);
   todoItem.innerHTML += `<i class="fa-solid fa-trash"></i>`;
   todoList.appendChild(todoItem);
+  todoInput.value = ""; 
+  todoInput.focus(); 
 }
 
+// Function to get todos from localStorage
+function getTodosFromLocalStorage() {
+  let todos=localStorage.getItem("todos") ? JSON.parse(localStorage.getItem("todos")) : [];
+  return todos;
+}
+
+// Function to add a todo to localStorage
+function addToDoLocalStorage(todo) {
+  let todos = getTodosFromLocalStorage();
+  todos.push(todo);
+  localStorage.setItem("todos", JSON.stringify(todos));
+
+  //console.log(todos);
+  //console.log(typeof todos);
+}
+
+// Function to load the app and display existing todos
+function loadApp() {
+    let todos = getTodosFromLocalStorage();
+
+    //console.log(todos);
+    todos.forEach((todo)=>{
+        createToDoListItem(todo);
+    });
+}
