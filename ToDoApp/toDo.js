76205824b5676dc16408e078b862220f8e70ee3f
Meryem Aydın <aydnn.meryem@gmail.com>
2025-07-31 15:29:35 +0300

    first commit

---
+++ ToDoApp/toDo.js
@@ -0,0 +1,9 @@
+//HTML elementlerini seçerek bir değişkene atadık
+const form = document.querySelector("#todo-form");
+const todoInput = document.querySelector("#todo-input");
+const addButton = document.querySelector("#add");
+const firstBody = document.querySelector("#first-body");
+const secondBody = document.querySelector("#second-body");
+const searchInput = document.querySelector("#search");
+const todoList = document.querySelector("#todo-list");
+const clearButton = document.querySelector("#clear");
